7c539636dcc7330e987a30329b055d2e2be6f5c7
Rohit-Kurmi <159707434+Rohit-Kurmi@users.noreply.github.com>
2024-09-27 13:58:19 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -4,9 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
-
-
+        # Dictionary to map closing to opening brackets
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        # Stack to keep track of the opening brackets
+        stack = []
+        
+        # Traverse through each character in the string
+        for char in s:
+            if char in bracket_map:
+                # Pop the top element from stack if it is non-empty, otherwise assign a dummy value '#'
+                top_element = stack.pop() if stack else '#'
+                
+                # If the top element doesn't match the corresponding opening bracket
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                # If it's an opening bracket, push it to the stack
+                stack.append(char)
+        
+        # At the end, the stack should be empty for a valid expression
+        return not stack
 
 
 
